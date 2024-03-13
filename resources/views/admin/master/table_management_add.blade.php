2d5b58714dba7b8fdfa4618195d61729a230716f
mujibansari <ansarimujeeb06@gmail.con>
2024-03-13 21:42:05 +0530

    Edit functiionality done

--- resources/views/admin/master/table_management_add.blade.php
+++ resources/views/admin/master/table_management_add.blade.php
@@ -78,30 +78,44 @@
 <script>
     var baseUrl = "{{url('/')}}";
     console.log(baseUrl);
-    $('#outlet_id').on('change', function() {
-        $('#outlet_department_id').html();
-        let outletId = this.value;
-        if( outletId != ''){
-            $.ajax({
-                url: baseUrl + '/admin/outlet-department-data/'+outletId,
-                type: 'GET',
-                success: function(resp) {
-                    let appendData = '<option value="">Select Outlets Department</option>';
-                    for (let index = 0; index < resp.length; index++) {
-                        appendData +=`<option value="${resp[index].id}">${resp[index].outlet_department_name}</option>`;
-                    }
-                    $('#outlet_department_id').html(appendData);
-                },
-                error: function(xhr, ajaxOptions, thrownError) {
-                    let appendData = '<option value="">Select Outlets Department</option>';
-                    $('#outlet_department_id').html();
-                    var errorMsg = 'Ajax request get outlet department data failed: ' + xhr.responseText;
-                    console.log(errorMsg)
-                }
-            });
-        } else {
+    var outletDepartMentId = "<?php echo isset($dataArray) ? $dataArray->outlet_department_id : '' ?>";
+    $(document).ready(function(){
+        $('#outlet_id').change(function() {
+            let outletId = this.value;
+            getOutletDepartmentData(outletId);
+        }).trigger('change');
 
+        function getOutletDepartmentData(outletId)
+        {
+            $('#outlet_department_id').html('');
+            if( outletId != ''){
+                $.ajax({
+                    url: baseUrl + '/admin/outlet-department-data/'+outletId,
+                    type: 'GET',
+                    success: function(resp) {
+                        let appendData = '<option value="">Select Outlets Department</option>';
+                        for (let index = 0; index < resp.length; index++) {
+                            let selected ='';
+                            if(resp[index].id == outletDepartMentId) {
+                                selected = 'selected';
+                            }
+                            appendData +=`<option value="${resp[index].id}" ${selected}>${resp[index].outlet_department_name}</option>`;
+                        }
+                        $('#outlet_department_id').html(appendData);
+                    },
+                    error: function(xhr, ajaxOptions, thrownError) {
+                        let appendData = '<option value="">Select Outlets Department</option>';
+                        $('#outlet_department_id').html(appendData);
+                        var errorMsg = 'Ajax request get outlet department data failed: ' + xhr.responseText;
+                        console.log(errorMsg)
+                    }
+                });
+            } else {
+                let appendData = '<option value="">Select Outlets Department</option>';
+                $('#outlet_department_id').html(appendData);
+            }
         }
     });
+
 </script>
 @endsection
